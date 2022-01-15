4785a7bd342cc14533a1c7fcc1ab0a54d4874c54
Akshay <rathodakshay@gmail.com>
2022-01-15 13:49:03 +0530

    ShuffelCard function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
+
+
+
+const cardImages = [
+  {"src":"/img/helmet-1.png"},
+  {"src":"/img/potion-1.png"},
+  {"src":"/img/ring-1.png"},
+  {"src":"/img/scroll-1.png"},
+  {"src":"/img/shield-1.png"},
+  {"src":"/img/sword-1.png"}
+]
 
 function App() {
+
+  const [cards,setCards]  = useState([]);
+  const [terns, setTerns] = useState(0);
+
+   //shuffel cards
+   const shuffelCards= ()=>{
+     const shuffelCards = [...cardImages, ...cardImages]
+     .sort(()=> Math.random()-0.5)
+     .map((card)=>({...card, id: Math.random() } ));
+
+      setCards(shuffelCards);
+      setTerns(0);
+   }
+
+   console.log(cards,terns);
+
+
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffelCards}>New Game</button>
     </div>
   );
 }
